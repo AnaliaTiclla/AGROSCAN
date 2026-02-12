0503e1ec24568e1abbaced719b2d8a5c12359959
Cristhian1611 <167483708+Cristhian1611@users.noreply.github.com>
2026-02-11 21:54:29 -0500

    Initial clean commit

---
+++ SOFTWARE/UTILS/clases.py
@@ -0,0 +1,11 @@
+# clases.py
+# Diccionario de normalización de clases para el modelo de espárragos
+
+CLASES_MAP = {
+    # Variaciones sin tilde y con tilde
+    "esparrago sano": "Espárrago sano",
+    "espárrago sano": "Espárrago sano",
+
+    "esparrago enfermo": "Espárrago enfermo",
+    "espárrago enfermo": "Espárrago enfermo",
+}
