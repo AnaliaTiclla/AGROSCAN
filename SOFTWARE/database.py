a17122f0e4bad412e23d4adb5a087ce8fb58071d
Albarran123 <146888051+Albarran123@users.noreply.github.com>
2026-02-16 15:40:00 -0500

    Delete SOFTWARE directory

--- SOFTWARE/database.py
+++
@@ -1,5 +0,0 @@
-# database.py  -> proxy hacia SQL Server
-# No cambies los imports en el resto de archivos: este módulo reexporta
-# las mismas funciones que usabas en SQLite, pero ahora desde SQL Server.
-
-from database_mssql import *  # noqa: F401,F403
